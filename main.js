beea7b9f7752735df17d3fc8ecd539f2b9ec0f26
aldi <aldinissa093@gmail.com>
2026-04-04 22:59:31 +0700

    fix array js

--- main.js
+++ main.js
@@ -10,3 +10,78 @@ function out() {
     overlay.style.display = "none"
     popup.style.display = "none"
 }
+
+const produk = [
+    {
+        nama: "laptop",
+        harga: "Rp9.000.000",
+        daerah: "Jawa Barat"
+    },
+    {
+        nama: "Kemeja",
+        harga: "Rp90.000",
+        daerah: "Maluku"
+    },
+    {
+        nama: "Ipad",
+        harga: "Rp5.000.000",
+        daerah: "Jakarta Pusat"
+    },
+    {
+        nama: "Kipas",
+        harga: "Rp70.000",
+        daerah: "Jakarta Pusat"
+    },
+    {
+        nama: "Charger type C",
+        harga: "Rp56.000",
+        daerah: "Semarang"
+    },
+    {
+        nama: "Kursi gaming",
+        harga: "Rp750.000",
+        daerah: "Jawa Barat"
+    },
+    {
+        nama: "Meja belajar",
+        harga: "Rp44.000",
+        daerah: "Jawa Tengah"
+    },
+    {
+        nama: "Iphone 17 promex",
+        harga: "Rp50.000.000",
+        daerah: "Jakarta Timur"
+    },
+    {
+        nama: "Tas sekolah",
+        harga: "Rp250.000",
+        daerah: "Jawa Timur"
+    },
+    {
+        nama: "Buku sejarah indo",
+        harga: "Rp50.000",
+        daerah: "Jakarta Pusat"
+    },
+    {
+        nama: "lampu belajar",
+        harga: "Rp90.000",
+        daerah: "Jawa Barat"
+    },
+    {
+        nama: "laptop lenovo",
+        harga: "Rp12.000.000",
+        daerah: "Jogja"
+    },
+    {
+        nama: "Sepeda",
+        harga: "Rp900.000",
+        daerah: "Banten"
+    },
+    {
+        nama: "Samsung smartphone",
+        harga: "Rp4.000.000",
+        daerah: "Riau"
+    },
+]
+
+const keranjang = []
