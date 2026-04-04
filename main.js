1bad3e3b8514d79c4a7b00b02defdfca3f03c056
aldi <aldinissa093@gmail.com>
2026-04-04 08:06:25 +0700

    fix DRY

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 const overlay = document.querySelector(".overlay")
+const popup = document.querySelector(".popup")
 
-function hover() {
-    overlay.style.display = "flex"
-}
+// function hover() {
+//     overlay.style.display = "flex"
+//     popup.style.display = "flex"
+// }
 
-function out() {
-    overlay.style.display = "none"
-}
+// function out() {
+//     overlay.style.display = "none"
+//     popup.style.display = "none"
+// }
+
+// function hoverr() {
+//     overlay.style.display = "flex"
+//     popup.style.display = "flex"
+// }
+
+// function outt() {
+//     overlay.style.display = "none"
+//     popup.style.display = "none"
+// }
